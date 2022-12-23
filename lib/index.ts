c60f6e7b05eb4c37a9823181766564a495051d6a
Jan Soukup <jan@ssoukup.com>
2022-12-23 16:33:51 +0100

    fix(output): correct naming of zips

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,6 @@
 import * as path from 'path'
 
 export const optimizerCodePath = path.resolve(__dirname, './code.zip')
-export const optimizerLayerPath = path.resolve(__dirname, './layer')
+export const optimizerLayerPath = path.resolve(__dirname, './dependencies-layer.zip')
 
 export const handler = 'imaginex-lambda/handler.handler'
