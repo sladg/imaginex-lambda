073f9ff91ecf3f51ef9ad289fcb880dbaac1c6fd
Petr Holík <petr.holik@ableneo.com>
2023-04-14 12:03:54 +0200

    refactor(tests,libs): added tests, readme, docstrings

--- imaginex_lambda/lib/utils.py
+++ imaginex_lambda/lib/utils.py
@@ -4,6 +4,8 @@ from urllib.parse import urlparse
 import filetype
 import logging
 
+from imaginex_lambda.lib.exceptions import HandlerError
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -23,12 +25,29 @@ def is_absolute(url: str) -> bool:
 
 
 def get_extension(buffer: IO[bytes]) -> Dict:
+    """
+    Determines the file extension and content type of an image file contained in the given buffer.
+
+    Args:
+        buffer (IO[bytes]): The buffer containing the image file.
+
+    Returns:
+        Dict[str, str]: A dictionary containing the content_type and extension of the image file.
+
+    Raises:
+        HandlerError: If the image format is not supported.
+    """
     logger.info("Getting extension...")
 
     kind = filetype.guess(buffer)
+    if kind is None:
+        raise HandlerError('Unsupported image format')
     content_type = kind.mime
     extension = content_type.upper().replace('IMAGE/', '')
-
+    if extension == 'JPX':
+        extension = 'JPEG2000'
+    elif extension == 'X-ICON':
+        extension = 'ICO'
     logger.info(f"Extension: {extension}")
     return {'content_type': content_type, 'extension': extension}
 
